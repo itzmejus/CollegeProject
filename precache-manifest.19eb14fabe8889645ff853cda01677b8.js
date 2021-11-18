ca1b1b1a544b08d125f904c05ff52ca36905398d
itzmejus <itzjustin3344@gmail.com>
2021-11-17 21:02:42 -0800

    Updates

---
+++ precache-manifest.19eb14fabe8889645ff853cda01677b8.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055ba1c5aa2bc27934baa16a53663092",
+    "url": "/CollegeProject/index.html"
+  },
+  {
+    "revision": "22d9c070d5dc4782873b",
+    "url": "/CollegeProject/static/css/2.d4dfd184.chunk.css"
+  },
+  {
+    "revision": "db1514a88b2dd7e2510e",
+    "url": "/CollegeProject/static/css/main.ccbc32cf.chunk.css"
+  },
+  {
+    "revision": "22d9c070d5dc4782873b",
+    "url": "/CollegeProject/static/js/2.e07e5e75.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/CollegeProject/static/js/2.e07e5e75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db1514a88b2dd7e2510e",
+    "url": "/CollegeProject/static/js/main.2e28af6c.chunk.js"
+  },
+  {
+    "revision": "2d44f27f3f9e7023af1e",
+    "url": "/CollegeProject/static/js/runtime-main.46510f19.js"
+  },
+  {
+    "revision": "df5c56cebf80a9db36404da2ae3ad989",
+    "url": "/CollegeProject/static/media/appliances.df5c56ce.jpg"
+  },
+  {
+    "revision": "c57f34c2fa6d65f5ee329a8270788d0e",
+    "url": "/CollegeProject/static/media/bestbrandphones.c57f34c2.jpg"
+  },
+  {
+    "revision": "e916840c4fb35b04d386727bf8f582d0",
+    "url": "/CollegeProject/static/media/boeimg2.e916840c.jpg"
+  },
+  {
+    "revision": "fd25d5fcfcc0da8cb97429998f9c7241",
+    "url": "/CollegeProject/static/media/boeimg3.fd25d5fc.jpg"
+  },
+  {
+    "revision": "f2f6f02ba6f2a67eead9624354576fe5",
+    "url": "/CollegeProject/static/media/boeimg4.f2f6f02b.jpg"
+  },
+  {
+    "revision": "844e84f5d20dcfe5d66ee7ed2d4f5036",
+    "url": "/CollegeProject/static/media/boeimg5.844e84f5.jpg"
+  },
+  {
+    "revision": "5fc34d1987b81a25e2150c38cdff268a",
+    "url": "/CollegeProject/static/media/boeimg6.5fc34d19.jpg"
+  },
+  {
+    "revision": "e112617aa3086a9475f28ca063cc4ce4",
+    "url": "/CollegeProject/static/media/camera.e112617a.png"
+  },
+  {
+    "revision": "419567f89844863ad837c76d369ad834",
+    "url": "/CollegeProject/static/media/ecommerce.419567f8.svg"
+  },
+  {
+    "revision": "3483452ecb00fdd85f74bd1450483b15",
+    "url": "/CollegeProject/static/media/electronics.3483452e.jpg"
+  },
+  {
+    "revision": "33b8aad6e281ab63a3b5ca223623083a",
+    "url": "/CollegeProject/static/media/hsimg1.33b8aad6.jpg"
+  },
+  {
+    "revision": "286e2c4995eb80165d20d166f5975aea",
+    "url": "/CollegeProject/static/media/hsimg2.286e2c49.jpg"
+  },
+  {
+    "revision": "a8bfe1289f5ccf871d1c591ae22b2f58",
+    "url": "/CollegeProject/static/media/hsimg3.a8bfe128.jpg"
+  },
+  {
+    "revision": "27a1589b724950c59d2073d874780ef5",
+    "url": "/CollegeProject/static/media/hsimg4.27a1589b.jpg"
+  },
+  {
+    "revision": "bd9d7883baee1e0632610ab71b36d5b9",
+    "url": "/CollegeProject/static/media/hsimg5.bd9d7883.jpg"
+  },
+  {
+    "revision": "f26a1e22563529f7d9f8e347c2e64637",
+    "url": "/CollegeProject/static/media/hsimg6.f26a1e22.jpg"
+  },
+  {
+    "revision": "052618d2859e58021869e939a52a585b",
+    "url": "/CollegeProject/static/media/hsimg7.052618d2.jpg"
+  },
+  {
+    "revision": "b7d27a62e806aa993ba56358639f0cc0",
+    "url": "/CollegeProject/static/media/hsimg8.b7d27a62.jpg"
+  },
+  {
+    "revision": "cf77a3c226681aeee95bf6d5d9ce95c0",
+    "url": "/CollegeProject/static/media/lap1.cf77a3c2.jpg"
+  },
+  {
+    "revision": "aebe5070c9137f4171dc3589abaca6f7",
+    "url": "/CollegeProject/static/media/lap2.aebe5070.jpg"
+  },
+  {
+    "revision": "863c1555240b199cfd0cfb67c05188c1",
+    "url": "/CollegeProject/static/media/logo.863c1555.jpg"
+  },
+  {
+    "revision": "a34ff770b961080c6cd1d0a213ac6f72",
+    "url": "/CollegeProject/static/media/power.a34ff770.png"
+  },
+  {
+    "revision": "d39e3dbad7ae0b69e617b7866105be8b",
+    "url": "/CollegeProject/static/media/slide_left.d39e3dba.JPG"
+  },
+  {
+    "revision": "a853b6873d901d08ad7739741d4e459e",
+    "url": "/CollegeProject/static/media/slide_right.a853b687.JPG"
+  },
+  {
+    "revision": "0ae5a95a537fb0f5fb9c7ae0364a177f",
+    "url": "/CollegeProject/static/media/sliderimg2.0ae5a95a.jpg"
+  },
+  {
+    "revision": "4a0823a7dbfb083a99b635d4f9b9f226",
+    "url": "/CollegeProject/static/media/sliderimg3.4a0823a7.jpg"
+  },
+  {
+    "revision": "145e89db3a3cf2b02ca9a34157068202",
+    "url": "/CollegeProject/static/media/sliderimg4.145e89db.jpg"
+  },
+  {
+    "revision": "b13352acd4b14c85a2967a0392aec813",
+    "url": "/CollegeProject/static/media/speaker11.b13352ac.png"
+  },
+  {
+    "revision": "e103dba7e78e6cc5c8635c1646ba30c1",
+    "url": "/CollegeProject/static/media/tooimg1.e103dba7.jpg"
+  },
+  {
+    "revision": "74102102adea25dbeef80f5c18d5045e",
+    "url": "/CollegeProject/static/media/tooimg2.74102102.jpg"
+  },
+  {
+    "revision": "82419d495f833d53cfcd337c8a19e92b",
+    "url": "/CollegeProject/static/media/tooimg3.82419d49.jpg"
+  },
+  {
+    "revision": "481ff3cd2d3116300b508a33195f62cf",
+    "url": "/CollegeProject/static/media/tooimg4.481ff3cd.jpg"
+  },
+  {
+    "revision": "da5ebfdba850e1de84658fead7d0a117",
+    "url": "/CollegeProject/static/media/tooimg6.da5ebfdb.jpg"
+  },
+  {
+    "revision": "a7b2b7a728e0d8beef9fecfeacb77058",
+    "url": "/CollegeProject/static/media/tooimg8.a7b2b7a7.jpg"
+  }
+]);
